5c9cb8126b6e9da3a82fcbfe65024a24e5cdc165
Liza Tennis and Victoria Martinez <liza@>
2018-08-15 16:06:31 -0700

    add if statement for slice function and add way to sentence

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 // Business Logic
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -5,22 +6,9 @@ var vowels = ["a", "e", "i", "o", "u"]
 var cons = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
 var pigLatin = []
 
-var pigLatin = function(sentence) {
-  for (var i = 0; i < letters.length; i++) {
-  var letter = letters[i];
-  for (var j = 0; j < vowels.length; i++) {
-    var vowel = vowels[j];
-  }
-console.log(sentence);
-console.log(letter);
-if (sentence === vowel) {
-  return 2;
-} else if (sentence === letter) {
-      console.log("true") 
-      return 1;
-  }
-}
-};
+
+
+
 
 
 
@@ -33,8 +21,30 @@ if (sentence === vowel) {
 $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
-    var sentence = ($("input#sentence").val());
-    var result = pigLatin(sentence);
-    $("#pigresult").text(result);
+
+    var sentence = $("input#sentence").val();
+    // var result = pigLatin(sentence);
+    var firstLetter = sentence.slice(0,1);
+    console.log(firstLetter);
+
+
+
+    // var pigLatin = function(sentence) {
+    for (var i = 0; i < vowels.length; i++) {
+      var vowel = vowels[i];
+      if (firstLetter === vowel) {
+        var result = sentence + "way";
+
+
+        $("#pigresult").text(result);
+      } else {firstLetter === vowel
+      }
+    }
   });
 });
+
+
+// for (var j = 0; j < vowels.length; i++) {
+//   var vowel = vowels[j];
+
+// console.log("true")
