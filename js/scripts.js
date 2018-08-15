ed41a5476c7387ed313f68b63504d609df213ee8
Liza Tennis and Victoria Martinez <liza@>
2018-08-15 13:35:16 -0700

    finish skeleton spec to return true for letters

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
 // Business Logic
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var vowels = ["a", "e", "i", "o", "u"]
+var cons = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
 var pigLatin = []
 
 var pigLatin = function(sentence) {
-
-  if (sentence !== letters) {
-    return false;
+  for (var i = 0; i < letters.length; i++) {
+  var letter = letters[i];
+console.log(sentence);
+console.log(letter);
+if (sentence === letter) {
+    console.log("true")
+    return true;
+  } else {
+    // return false;
   }
+}
 };
-
 
 
 
@@ -22,7 +30,7 @@ var pigLatin = function(sentence) {
 $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
-    var sentence = parseInt($("input#sentence").val());
+    var sentence = ($("input#sentence").val());
     var result = pigLatin(sentence);
     $("#pigresult").text(result);
   });
